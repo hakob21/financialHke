9ce34d4f5f4e0a732f111cc7cefbfd11701194c3
hakob21 <hakob21@gmail.com>
2024-03-31 18:04:28 +0200

    add comment and comment out test

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-import ComposeApp
+import ComposeApp // pay attention to the uppercase in ComposeApp. In reality composeApp is a package with lowercase
 
 @main
 struct iOSApp: App {
