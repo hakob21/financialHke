e0fe891d9e2a27733fc18308ee9b5e94337f925b
hakob21 <hakob21@gmail.com>
2024-04-07 12:32:34 +0200

    add body to the DriverFactory's method of ios actual impl

--- composeApp/src/iosMain/kotlin/com/hakob/financialhke/db/dbconfig/DatabaseDriverFactory.ios.kt
+++ composeApp/src/iosMain/kotlin/com/hakob/financialhke/db/dbconfig/DatabaseDriverFactory.ios.kt
@@ -4,9 +4,8 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import com.hakob.financialhke.database.Database
 
-// don't need this for current impl. Depending on how will refactor the sqldelight initialization, might need it later
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
-        TODO("Not yet implemented")
+        return NativeSqliteDriver(Database.Schema, "test.db")
     }
 }
